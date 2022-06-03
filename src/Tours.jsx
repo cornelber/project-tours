cf718c1a3776ed2e7873893a517f7c2dc94848d6
cornelber <bercornel@gmail.com>
2022-06-03 17:02:46 +0300

    setup tours list section

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,7 +1,23 @@
+import './App.css'
 import React from 'react'
+import Tour from './Tour'
 
-const Tours = () => {
-	return <div>Tours</div>
+const Tours = ({ tours, removeTour }) => {
+	return (
+		<section className='tours-section'>
+			<div className='tours-title'>
+				<h2>Our Tours</h2>
+				<div className='tours-title__line'></div>
+			</div>
+			<div className='tours-list'>
+				{tours.map(tour => {
+					return (
+						<Tour key={tour.id} {...tour} removeTour={removeTour} />
+					)
+				})}
+			</div>
+		</section>
+	)
 }
 
 export default Tours
