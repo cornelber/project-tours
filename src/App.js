5e8dc03844bad5966b3fb590cc9e0bc62603da23
cornelber <bercornel@gmail.com>
2022-06-03 17:01:19 +0300

    setup app.js and styles for webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
-import { useState, useEffect } from 'react'
-import Tour from './Tour'
-import Tours from './Tours'
 import './App.css'
+import { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
 
 const url = 'https://course-api.com/react-tours-project'
 
 function App() {
-	return <div className='App'></div>
+	const [loading, setLoading] = useState(true)
+	const [tours, setTours] = useState([])
+
+	const removeTour = id => {
+		const newTours = tours.filter(tour => tour.id !== id)
+		setTours(newTours)
+	}
+
+	const handleTours = () => {
+		fetchData()
+	}
+
+	const fetchData = () => {
+		fetch(url).then(res =>
+			res.json().then(data => {
+				try {
+					setLoading(false)
+					setTours(data)
+				} catch (error) {
+					setLoading(false)
+					console.log(error)
+				}
+			})
+		)
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		)
+	}
+
+	if (tours.length === 0) {
+		return (
+			<main>
+				<div className='tours-title'>
+					<h2>No Tours Find</h2>
+					<div className='tours-title__line'></div>
+					<button onClick={handleTours} className='btn-refresh'>
+						Refresh
+					</button>
+				</div>
+			</main>
+		)
+	}
+
+	return (
+		<main>
+			<Tours tours={tours} removeTour={removeTour} />
+		</main>
+	)
 }
 
 export default App
