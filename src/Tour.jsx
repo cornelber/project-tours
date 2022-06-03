61f4fe8e312bdce3c01162378950d90793662add
cornelber <bercornel@gmail.com>
2022-06-03 17:02:28 +0300

    setup tour item

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,7 +1,43 @@
+import './App.css'
 import React from 'react'
+import { useState } from 'react'
 
-const Tour = () => {
-	return <div>Tour</div>
+const Tour = ({ id, name, info, image, price, removeTour }) => {
+	const [readMore, setReadMore] = useState(false)
+
+	const handleInfo = () => {
+		setReadMore(!readMore)
+	}
+
+	return (
+		<article>
+			<div className='tour-img'>
+				<img src={image} alt={name} />
+			</div>
+			<footer className='tour-footer'>
+				<div className='tour-title'>
+					<h2>{name}</h2>
+					<div className='tour-price'>
+						<p>${price}</p>
+					</div>
+				</div>
+				<div className='tour-subtitle'>
+					<p>
+						{readMore ? info : `${info.substring(0, 200)}...`}
+						<button
+							onClick={handleInfo}
+							className='tour-subtitle__btn'
+						>
+							{readMore ? 'Show Less' : 'Read More'}
+						</button>
+					</p>
+				</div>
+				<button className='tour-btn' onClick={() => removeTour(id)}>
+					Not Interested
+				</button>
+			</footer>
+		</article>
+	)
 }
 
 export default Tour
