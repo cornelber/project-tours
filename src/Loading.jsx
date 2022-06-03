be30bba7facf160dc0f1960190835606b9b4bd71
cornelber <bercornel@gmail.com>
2022-06-03 17:01:56 +0300

    setup Loading section

---
+++ src/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './App.css'
+
+const Loading = () => {
+	return (
+		<section className='loading-section'>
+			<h1 className='loading-title'>Loading...</h1>
+		</section>
+	)
+}
+
+export default Loading
